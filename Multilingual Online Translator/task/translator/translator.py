c44c8663685a4175da26606f5642fe6fd9b56c7c
Tomasz Drozdowski <tomasz.drozdowski@op.pl>
2022-08-03 23:14:23 +0200

    Stage 4/7: All of them

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,39 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
 
-print('Type "en" if you want to translate from French into English, or "fr" if'
-      'you want to translate from English into \French:')
-target_lang = input()
-if target_lang == 'en':
-    translation_dir = 'french-english'
-else:
-    translation_dir = 'english-french'
-target_lang_full = translation_dir[:translation_dir.find('-')]
+languages = ['Arabic',
+             'German',
+             'English',
+             'Spanish',
+             'French',
+             'Hebrew',
+             'Japanese',
+             'Dutch',
+             'Polish',
+             'Portuguese',
+             'Romanian',
+             'Russian',
+             'Turkish']
+
+print('Hello, welcome to the translator. Translator supports:')
+for i, language in enumerate(languages, 1):
+    print(f'{i}. {language}')
+
+print('Type the number of your language:')
+source_lang = languages[int(input())-1]
+print('Type the number of language you want to translate to:')
+target_lang = languages[int(input())-1]
+
+translation_dir = f'{source_lang}-{target_lang}'.lower()
 
 print('Type the word you want to translate:')
 word = input()
 
-print(f'You chose "{target_lang}" as a language to translate "{word}".')
+
 
 headers = {'User-Agent': 'Mozilla/5.0'}
 while True:
     r = requests.get(f'https://context.reverso.net/translation/{translation_dir}/{word}', headers=headers)
     if r.status_code == 200:
-        print(f'{r.status_code} OK')
+        # print(f'{r.status_code} OK')
         break
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
-print(f'{target_lang_full.capitalize()} Translations')
+print(f'\n{target_lang.capitalize()} Translations:')
 
 # Translations
 translations = []
-for translation in soup.find_all('a', {'class': 'translation', 'class': 'ltr', 'class': 'dict'}):
+for translation in soup.find_all('a', {'class': 'translation'}):
+    # print(translation)
     translations.append(translation.get('data-term'))
     print(translations[-1])
 
 # Sentences
-print(f'\n{target_lang_full.capitalize()} Examples')
+print(f'\n{target_lang.capitalize()} Examples:')
 examples = []
 for example in soup.find_all('div', {'class': 'src ltr'}):
     if not example.parent.get('class') is None:
@@ -46,8 +63,8 @@ for example_trg in soup.find_all('div', {'class': 'trg ltr'}):
         if example_trg.parent.get('class')[0] == 'example':
             examples[i][1] = example_trg.text.strip()
 
+            print(f'{examples[i][1]}:')
             print(examples[i][0])
-            print(examples[i][1])
             print()
             i = i + 1
 

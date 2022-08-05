86741c6841ce06aca3a44087161b1844111fca42
Tomasz Drozdowski <tomasz.drozdowski@op.pl>
2022-08-05 18:19:16 +0200

    Stage 5/7: Simultaneous translation

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-languages = ['Arabic',
+languages = ['all',
+             'Arabic',
              'German',
              'English',
              'Spanish',
@@ -16,55 +17,72 @@ languages = ['Arabic',
              'Turkish']
 
 print('Hello, welcome to the translator. Translator supports:')
-for i, language in enumerate(languages, 1):
-    print(f'{i}. {language}')
+for i, language in enumerate(languages):
+    if i != 0:
+        print(f'{i}. {language}')
 
 print('Type the number of your language:')
-source_lang = languages[int(input())-1]
-print('Type the number of language you want to translate to:')
-target_lang = languages[int(input())-1]
+source_lang = languages[int(input())]
+print('Type the number of a language you want to translate to or "0" to translate to all languages:')
+target_lang_number = int(input())
 
-translation_dir = f'{source_lang}-{target_lang}'.lower()
+if target_lang_number == 0:
+    target_langs = languages[1:]
+else:
+    target_langs = languages[target_lang_number:target_lang_number + 1]
 
 print('Type the word you want to translate:')
 word = input()
 
+# Utworzenie pliku
+with open(f'{word}.txt', 'w', encoding='utf-8') as file:
 
+    for target_lang in target_langs:
+        if target_lang == source_lang:
+            continue
 
-headers = {'User-Agent': 'Mozilla/5.0'}
-while True:
-    r = requests.get(f'https://context.reverso.net/translation/{translation_dir}/{word}', headers=headers)
-    if r.status_code == 200:
-        # print(f'{r.status_code} OK')
-        break
+        translation_dir = f'{source_lang}-{target_lang}'.lower()
 
-soup = BeautifulSoup(r.content, 'html.parser')
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        while True:
+            r = requests.get(f'https://context.reverso.net/translation/{translation_dir}/{word}', headers=headers)
+            if r.status_code == 200:
+                # print(f'{r.status_code} OK')
+                break
 
-print(f'\n{target_lang.capitalize()} Translations:')
+        soup = BeautifulSoup(r.content, 'html.parser')
 
-# Translations
-translations = []
-for translation in soup.find_all('a', {'class': 'translation'}):
-    # print(translation)
-    translations.append(translation.get('data-term'))
-    print(translations[-1])
+        print(f'\n{target_lang.capitalize()} Translations:')
+        file.write(f'\n{target_lang.capitalize()} Translations:\n')
 
-# Sentences
-print(f'\n{target_lang.capitalize()} Examples:')
-examples = []
-for example in soup.find_all('div', {'class': 'src ltr'}):
-    if not example.parent.get('class') is None:
-        if example.parent.get('class')[0] == 'example':
-            examples.append([example.text.strip(), ''])
+        # Translations
+        translations = []
+        for translation in soup.find_all('a', {'class': 'translation'}):
+            # print(translation)
+            if not translation.get('data-term') is None:
+                translations.append(translation.get('data-term'))
+                print(translations[-1])
+                file.write(f'{translations[-1]}\n')
 
-i = 0
-for example_trg in soup.find_all('div', {'class': 'trg ltr'}):
-    if not example_trg.parent.get('class') is None:
-        if example_trg.parent.get('class')[0] == 'example':
-            examples[i][1] = example_trg.text.strip()
+        # Sentences
+        print(f'\n{target_lang.capitalize()} Examples:')
+        file.write(f'\n{target_lang.capitalize()} Examples:\n')
+        examples = []
+        for example in soup.find_all('div', {'class': 'src'}):
+            if not example.parent.get('class') is None:
+                if example.parent.get('class')[0] == 'example':
+                    examples.append([example.text.strip(), ''])
 
-            print(f'{examples[i][1]}:')
-            print(examples[i][0])
-            print()
-            i = i + 1
+        i = 0
+        for example_trg in soup.find_all('div', {'class': 'trg'}):
+           if not example_trg.parent.get('class') is None:
+                if example_trg.parent.get('class')[0] == 'example':
+                    examples[i][1] = example_trg.text.strip()
 
+                    if i == 0:
+                        print(f'{examples[i][1]}:')
+                        file.write(f'{examples[i][1]}:\n')
+                        print(examples[i][0])
+                        file.write(f'{examples[i][0]}\n\n')
+                        print()
+                    i = i + 1
